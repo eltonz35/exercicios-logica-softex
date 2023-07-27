2bf582de330c899d4a6571394d7f1cbaf7f6bb38
Elthon <eltonsilva.mecatronic@gmail.com>
2023-07-26 22:23:21 -0300

    Exercicio 07

---
+++ exercicio07.js
@@ -0,0 +1,10 @@
+/**
+ * Faça um programa que imprima os números pares de 1 a 20.
+ */
+
+console.log("Os numeros pares são: ");
+for(i = 1; i <= 20 ; i++) {
+    if(i % 2 == 0) {
+        console.log(i)
+    }
+}

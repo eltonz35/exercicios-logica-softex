fdeac53d8237528a8ebecabb037e9b552a880209
Elthon <eltonsilva.mecatronic@gmail.com>
2023-07-26 22:23:14 -0300

    Exercicio 06

---
+++ exercicio06.js
@@ -0,0 +1,11 @@
+/**
+ * Crie um programa que imprima os números de 1 a 10 em ordem crescente.
+ */
+
+let numero = 10;
+
+console.log("Ordem crscente dos numeros: ");
+
+for(i = 0; i <= numero; i++) {
+    console.log(i);
+}

fef6ce23ba9ac9a945816b8de9629d55871b4e8f
Elthon <eltonsilva.mecatronic@gmail.com>
2023-07-26 23:27:12 -0300

    Exercicio 13

---
+++ exercicio13.js
@@ -0,0 +1,11 @@
+/**
+ * Crie um programa que inverta a ordem dos elementos em uma lista.
+ */
+
+const lista = [1, 2, 3, 4, 5, 6];
+
+console.log(`Lista inicial: ${lista}`)
+
+lista.reverse();
+
+console.log(`Lista invertida: ${lista}`);
